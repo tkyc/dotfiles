163ba9191424494a5d41ae2a0f525a036a06d4b3
tkyc <terrykyc.7000@gmail.com>
2020-04-22 16:52:04 -0700

    Window outline border;icons

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,6 +8,7 @@ cp ~/Scripts/* .
 # Update dotfiles
 cp ~/.bash_profile ../
 cp ~/.bashrc ../
+cp ~/.i3blocks.conf ../
 cp ~/.config/i3/config ../
 cp ~/.config/i3status/i3status.conf ../
 cp ~/.tmux.conf ../
