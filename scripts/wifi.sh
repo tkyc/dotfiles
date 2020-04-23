512d4e222d8028117f0871e94a56cc6ca1f0f740
tkyc <terrykyc.7000@gmail.com>
2020-04-22 20:30:50 -0700

    ownership/group change for connect.sh; added local ip to bar; decreased wifi refresh interval

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -2,9 +2,10 @@
 # Gets current wifi status
 
 wifi_status=$(cat /sys/class/net/wlp3s0/carrier)
+local_ip=$(ip addr | awk 'FNR == 11 {print $2}')
 
 if (("$wifi_status" == 1));then
-    echo "<span background='#008080'>  </span>"
+    echo "<span background='#008080'>  $local_ip </span>"
 fi
 
 echo "<span background='#008080'> 睊 </span>"
