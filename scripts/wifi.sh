714548e98a8ad025eff0d513d7f76e5bbed0e784
tkyc <terrykyc.7000@gmail.com>
2020-04-23 22:47:12 -0700

    fixes

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -2,10 +2,11 @@
 # Gets current wifi status
 
 wifi_status=$(cat /sys/class/net/wlp3s0/carrier)
-local_ip=$(ip addr | awk 'FNR == 11 {print $2}')
+local_ip=$(ip addr | awk '/wlp3s0$/ {print $2}')
 
 if (("$wifi_status" == 1));then
     echo "<span background='#008080'>  $local_ip </span>"
+else
+    echo "<span background='#008080'> 睊 </span>"
 fi
 
-echo "<span background='#008080'> 睊 </span>"
