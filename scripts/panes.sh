58b1ae09bc86596fe4009d5c8a2702d963ef72a1
tkyc <terrykyc.7000@gmail.com>
2020-05-03 11:37:09 -0700

    Script changes; screensaver disable move to xinitirc

--- scripts/panes.sh
+++ scripts/panes.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Tmux pane setup
 
+# 2 windows
+tmux rename-window Editor
+tmux new-window
+tmux select-window -t 0
+
 # 2 panes
-tmux split-window -v
-tmux resize-pane -y 8
-tmux select-pane -t 0
+#tmux split-window -v
+#tmux resize-pane -y 8
+#tmux select-pane -t 0
 
 # 3 panes
 #tmux split-window -v
--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -5,8 +5,8 @@ wifi_status=$(cat /sys/class/net/wlp3s0/carrier)
 local_ip=$(ip addr | awk '/wlp3s0$/ {print $2}')
 
 if (("$wifi_status" == 1));then
-    echo "<span background='#008080'>  $local_ip </span>"
+    echo "<span background='#008080'>  $local_ip <span foreground='#1b2631'></span></span>"
 else
-    echo "<span background='#008080'> 睊 </span>"
+    echo "<span background='#008080'> 睊 <span foreground='#1b2631'></span></span>"
 fi
 
