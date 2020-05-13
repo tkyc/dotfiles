2f9937bfc2eddb2fcc3e90afec64c841f5074207
tkyc <terrykyc.7000@gmail.com>
2020-05-12 18:36:29 -0700

    Added flag for code script

--- scripts/code.sh
+++ scripts/code.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 # Tmux pane setup
 
-# 2 windows
+# Default-----------------------------------------
+
+# Flags
+p_flag="-p"
+
 tmux rename-window -t 0 "CodeEditor []"
-tmux new-window
-tmux rename-window -t 1 "Shell "
-tmux select-window -t 0
+
+if [[ "$1" == "$p_flag" ]]; then
+    tmux split-window -h
+    tmux select-pane -t 1
+    tmux resize-pane -x 50
+    tmux select-pane -t 0
+else
+    tmux new-window
+    tmux rename-window -t 1 "Shell "
+    tmux select-window -t 0
+
+fi
+
 vim .
 
-# 2 panes
+#-------------------------------------------------
+
+# 2 panes horizontal
 #tmux split-window -v
 #tmux resize-pane -y 8
 #tmux select-pane -t 0
 
-# 3 panes
+# 3 panes horizontal
 #tmux split-window -v
 #tmux split-window -h
 #tmux resize-pane -y 9
