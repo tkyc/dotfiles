699c0ec385f68cca605febea4cb12b304d593556
tkyc <terrykyc.7000@gmail.com>
2021-04-04 20:57:06 -0700

    Tuned settings

--- scripts/memory.sh
+++ scripts/memory.sh
@@ -2,4 +2,4 @@
 # Get the amount of memory currently being used
 
 used_memory=$(free -h | grep Mem | awk '{print $3}')
-echo "<span background='#4b846c'><span background='#008b8b'>  </span> $used_memory </span>"
+echo "<span background='#4b846c'><span background='#4285F4'>  </span> $used_memory </span>"
