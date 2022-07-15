f4d91a5c81e5f2ef8a35e681ee8f5ba540a0c17c
tkyc <terrykyc.7000@gmail.com>
2022-07-14 22:34:20 -0700

    Update script updates to new dotfiles dirs

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,14 +11,14 @@ fi
 cp /home/$user/Scripts/* /home/$user/Repos/auto_rice_dotfiles/scripts
 
 # Update dotfiles
-cp /home/$user/.bash_profile /home/$user/Repos/auto_rice_dotfiles
-cp /home/$user/.bashrc /home/$user/Repos/auto_rice_dotfiles
-cp /home/$user/.config/gtk-3.0/settings.ini /home/$user/Repos/auto_rice_dotfiles
-cp /home/$user/.tmux.conf /home/$user/Repos/auto_rice_dotfiles
-cp /home/$user/.vimrc /home/$user/Repos/auto_rice_dotfiles
-cp /home/$user/.vim/colors/profile.vim /home/$user/Repos/auto_rice_dotfiles
-cp /home/$user/.config/nvim/init.vim /home/$user/Repos/auto_rice_dotfiles
-cp /home/$user/.config/nvim/coc.vim /home/$user/Repos/auto_rice_dotfiles
-cp /home/$user/.Xdefaults /home/$user/Repos/auto_rice_dotfiles
-cp /home/$user/.xinitrc /home/$user/Repos/auto_rice_dotfiles
+cp /home/$user/.bash_profile /home/$user/Repos/auto_rice_dotfiles/bash
+cp /home/$user/.bashrc /home/$user/Repos/auto_rice_dotfiles/bash
+cp /home/$user/.config/gtk-3.0/settings.ini /home/$user/Repos/auto_rice_dotfiles/gtk
+cp /home/$user/.tmux.conf /home/$user/Repos/auto_rice_dotfiles/tmux
+cp /home/$user/.vimrc /home/$user/Repos/auto_rice_dotfiles/nvim
+cp /home/$user/.vim/colors/profile.vim /home/$user/Repos/auto_rice_dotfiles/nvim
+cp /home/$user/.config/nvim/init.vim /home/$user/Repos/auto_rice_dotfiles/nvim
+cp /home/$user/.config/nvim/coc.vim /home/$user/Repos/auto_rice_dotfiles/nvim
+cp /home/$user/.Xdefaults /home/$user/Repos/auto_rice_dotfiles/x11
+cp /home/$user/.xinitrc /home/$user/Repos/auto_rice_dotfiles/x11
 
