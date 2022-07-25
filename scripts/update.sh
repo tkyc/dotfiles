cd5d9702d56f1be12c70597f4f088d7a4b3beb6e
tkyc <terrykyc.7000@gmail.com>
2022-07-24 22:28:43 -0700

    Updated repo name to dotfiles; added Nord theme

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,17 +8,17 @@ then
 fi
 
 # Update scripts
-cp /home/$user/Scripts/* /home/$user/Repos/auto_rice_dotfiles/scripts
+cp /home/$user/Scripts/* /home/$user/Repos/dotfiles/scripts
 
 # Update dotfiles
-cp /home/$user/.bash_profile /home/$user/Repos/auto_rice_dotfiles/bash
-cp /home/$user/.bashrc /home/$user/Repos/auto_rice_dotfiles/bash
-cp /home/$user/.config/gtk-3.0/settings.ini /home/$user/Repos/auto_rice_dotfiles/gtk
-cp /home/$user/.tmux.conf /home/$user/Repos/auto_rice_dotfiles/tmux
-cp /home/$user/.vimrc /home/$user/Repos/auto_rice_dotfiles/nvim
-cp /home/$user/.vim/colors/profile.vim /home/$user/Repos/auto_rice_dotfiles/nvim
-cp /home/$user/.config/nvim/init.vim /home/$user/Repos/auto_rice_dotfiles/nvim
-cp /home/$user/.config/nvim/coc.vim /home/$user/Repos/auto_rice_dotfiles/nvim
-cp /home/$user/.Xdefaults /home/$user/Repos/auto_rice_dotfiles/x11
-cp /home/$user/.xinitrc /home/$user/Repos/auto_rice_dotfiles/x11
+cp /home/$user/.bash_profile /home/$user/Repos/dotfiles/bash
+cp /home/$user/.bashrc /home/$user/Repos/dotfiles/bash
+cp /home/$user/.config/gtk-3.0/settings.ini /home/$user/Repos/dotfiles/gtk
+cp /home/$user/.tmux.conf /home/$user/Repos/dotfiles/tmux
+cp /home/$user/.vimrc /home/$user/Repos/dotfiles/nvim
+cp /home/$user/.vim/colors/profile.vim /home/$user/Repos/dotfiles/nvim
+cp /home/$user/.config/nvim/init.vim /home/$user/Repos/dotfiles/nvim
+cp /home/$user/.config/nvim/coc.vim /home/$user/Repos/dotfiles/nvim
+cp /home/$user/.Xdefaults /home/$user/Repos/dotfiles/x11
+cp /home/$user/.xinitrc /home/$user/Repos/dotfiles/x11
 
