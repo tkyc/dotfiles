7565c546f061979aec17210cc978cefd7ffe585e
tkyc <terrykyc.7000@gmail.com>
2026-02-08 21:10:23 -0800

    update.sh script toggle to update between repo and system

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,7 +8,16 @@ then
     user=$SUDO_USER
 fi
 
-# Update dotfiles
+# Update dotfiles in system
+update="u"
+if [ "$1" == "$update" ];then
+    cp /home/$user/repo/dotfiles/bash/.bashrc /home/$user/
+    cp -r /home/$user/repo/dotfiles/nvim/ /home/$user/.config/
+    cp /home/$user/repo/dotfiles/foot/foot.ini /home/$user/.config/foot/
+    cp /home/$user/repo/dotfiles/tmux/.tmux.conf /home/$user/
+fi
+
+# Update dotfiles in repo
 cp /home/$user/.bashrc /home/$user/repo/dotfiles/bash/
 cp -r /home/$user/.config/nvim/ /home/$user/repo/dotfiles/
 cp /home/$user/.config/foot/foot.ini /home/$user/repo/dotfiles/foot/
