8b5b3eade161bcd39842b4874aba6508a4eada01
tkyc <terrykyc.7000@gmail.com>
2026-02-10 09:47:54 -0800

    Added tcpdump a package to install in ansible playbook task

--- scripts/network.sh
+++ scripts/network.sh
@@ -6,7 +6,7 @@ local_ip=$(ip addr | awk '/wlp3s0$/ {print $2}')
 
 #if (("$wifi_status" == 1));then
 if [ ! -z $local_ip ];then
-#    echo "<span background='#4b846c'>  $local_ip <span foreground='#1b2631'></span></span>"
+    # echo "<span background='#4b846c'>  $local_ip <span foreground='#1b2631'></span></span>"
     echo "<span background='#4b846c'><span background='#0F9D58'>  </span> $local_ip </span>"
 else
     local_ip=$(ip addr | awk '/enp0s3$/ {print $2}')
