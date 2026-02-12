a52fa7088577222c6543e3e14c4babfbe3db8307
tkyc <terrykyc.7000@gmail.com>
2026-02-11 18:39:15 -0800

    Updated connect script to document 8021x AP connection

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 #
-# Add the configuration below to /etc/iwd/main.conf
+# To enable, add the configuration below to /etc/iwd/main.conf
 #
 # [General]
 # EnableNetworkConfiguration=true
 #
+# For 8021x APs, create a file with the contents below
+# with the .8021x extension eg. your-ssid.8021.x in
+# the directory /var/lib/iwd/
+#
+# [Security]
+# EAP-Method=PEAP
+# EAP-Identity=anonymous
+# EAP-PEAP-Phase2-Method=MSCHAPV2
+# EAP-PEAP-Phase2-Identity=<username>
+# EAP-PEAP-Phase2-Password=<password>
+# 
+# [Settings]
+# AutoConnect=true
+#
 
 echo "sudo iwctl known-networks list"
 echo "sudo iwctl known-networks <SSID> forget"
-echo "sudo iwctl station device list"
+echo "sudo iwctl device list"
+echo "sudo iwctl station <adapter> scan"
 echo "sudo iwctl station <adapter> get-networks"
 echo "sudo iwctl station <adapter> connect <SSID>"
 
